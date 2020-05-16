85e761ef78f411f85f1f7709e877f330a1d1de37
Mallik arjuna sharma R <mallik@Malliks-MacBook-Air.local>
2020-05-16 11:47:06 +0530

    included gallery

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,35 +1,26 @@
 import React from "react";
 import { Typography, Grid, Paper } from "@material-ui/core";
-import stringConstants from './stringConstants.js'
-import CHAIRMAN_MSG from './assests/chairman_msg.jpeg'
-import SEC_MSG from './assests/sec_msg.jpeg'
-import FACULTY from './assests/falculty.jpeg'
+import stringConstants from "./stringConstants.js";
+import AboutUs from './AboutUs';
+import Gallery from './Gallery';
 
 
-function Dashboard(){
+function Dashboard() {
+  return (
+    <Grid container justify="center" direction="column" spacing={2}>
 
-
-
-    return (
-      <Grid container justify="center">
-        <Grid item>
-          <Typography variant="h3" style={{ color: stringConstants.APP_COLOR }}>
-            About Us
-          </Typography>
-        </Grid>
-
-
-        <Grid item md={12} lg={12}>
-         <Grid container justify="center" spacing={2} style={{height:"500px"}}>
-          <Grid item md={4} lg={4}><img src={CHAIRMAN_MSG}  style={{height:"100%",width:"100%"}} /> </Grid>
-          <Grid item md={4} lg={4}><img src={SEC_MSG}  style={{height:"100%",width:"100%"}} /></Grid>
-          <Grid item md={4} lg={4}><img src={FACULTY}  style={{height:"100%",width:"100%"}} /></Grid>
-          </Grid>
-        </Grid>
-
+      <Grid item md={12} lg={12} style={{padding:0,margin:0}} >
+        <Gallery />
       </Grid>
-    );
 
+      <Grid item md={12} lg={12} >
+        <AboutUs />
+      </Grid>
+
+    
+     
+    </Grid>
+  );
 }
 
 export default Dashboard;
---
+++ src/Gallery.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Typography, Grid, Paper } from "@material-ui/core";
+import { Zoom } from 'react-slideshow-image';
+ import './Gallery.css';
+ import CHAIRMAN_MSG from './assests/chairman_msg.jpeg'
+ import SEC_MSG from './assests/sec_msg.jpeg'
+ import FACULTY from './assests/falculty.jpeg'
+
+ import Gallery1 from './assests/gallery_img1.jpeg'
+
+
+const images = [
+    Gallery1,
+    SEC_MSG,
+    FACULTY
+];
+
+
+const zoomOutProperties = {
+    duration: 5000,
+    transitionDuration: 500,
+    infinite: true,
+    indicators: true,
+    scale: 0.4,
+    arrows: true
+  }
+
+
+function Gallery(){
+
+
+
+    return (
+      <Grid item justify="center" md={12} lg={12} style={{width:"100%",height:"400px",padding:0,margin:0,marginBottom:"10%"}}>
+       
+       <div className="slide-container">
+        <Zoom {...zoomOutProperties}>
+          {
+            images.map((each, index) => <img key={index} style={{width: "100%",height:"100%"}} src={each} />)
+          }
+        </Zoom>
+      </div>
+
+      </Grid>
+    );
+
+}
+
+export default Gallery;
